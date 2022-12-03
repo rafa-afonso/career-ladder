ea2c97120f5925e596de1fddc5624aacbc8a01a3
rafa-afonso <rafael.s.afonso@gmail.com>
2022-12-03 11:13:53 -0300

    Cria docker-compose específico para execução nativa

--- docker-compose-env.sh
+++ docker-compose-env.sh
@@ -3,9 +3,15 @@
 # Script para selecionar o .env correto no docker compose
 # Exemplo de uso: ./docker-compose-env.sh up --build -d
 
-CONFIG=.env.dev
-# CONFIG=.env.stage
-# CONFIG=.env.prod
+# Nota: Para a execução Native o --build deve ser feito a parte
+# Build Native: mvn -Pnative spring-boot:build-image
 
-COMMAND="docker compose --env-file ./environment/$CONFIG"
-$COMMAND $@
+ENV_VAR=.env.dev
+# ENV_VAR=.env.stage
+# ENV_VAR=.env.prod
+
+COMPOSE_PATH=docker-compose.yml
+# COMPOSE_PATH=docker-compose-native.yml
+
+COMMAND="docker compose -f $COMPOSE_PATH --env-file ./environment/$ENV_VAR"
+$COMMAND "$@"
