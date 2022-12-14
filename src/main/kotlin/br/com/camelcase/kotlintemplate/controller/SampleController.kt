c28a425fc76637dd27dba11cb76197e6829155bb
rafa-afonso <rafael.s.afonso@gmail.com>
2022-12-14 19:53:46 -0300

    Configura logger e níveis de acesso

--- src/main/kotlin/br/com/camelcase/kotlintemplate/controller/SampleController.kt
+++ src/main/kotlin/br/com/camelcase/kotlintemplate/controller/SampleController.kt
@@ -1,15 +1,25 @@
 package br.com.camelcase.kotlintemplate.controller
 
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+// Place definition above class declaration to make field static
+private val log = KotlinLogging.logger {}
+
 @RestController
-@RequestMapping("/docker")
+@RequestMapping("/")
 class SampleController {
 
     @GetMapping
     fun sampleGet(): String {
+        val logTest = "Log qualquer"
+        log.error { "$logTest | error log" }
+        log.warn { "$logTest | warn log" }
+        log.info { "$logTest | info log" }
+        log.debug { "$logTest | debug log" }
+        log.trace { "$logTest | trace log" }
         return "Hello camelCase World!"
     }
 
