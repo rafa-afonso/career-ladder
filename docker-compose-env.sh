0c9a8e81d7c660773f2a79672cf52b8c7d6b1464
rafa-afonso <rafael.s.afonso@gmail.com>
2022-12-22 18:54:13 -0300

    Adiciona variável de ambiente para controlar log level

--- docker-compose-env.sh
+++ docker-compose-env.sh
@@ -3,9 +3,9 @@
 # Script para selecionar o .env correto no docker compose
 # Exemplo de uso: ./docker-compose-env.sh up --build -d
 
-CONFIG=.env.dev
+# CONFIG=.env.dev
 # CONFIG=.env.stage
-# CONFIG=.env.prod
+CONFIG=.env.prod
 
 COMMAND="docker compose --env-file ./environment/$CONFIG"
 $COMMAND "$@"
